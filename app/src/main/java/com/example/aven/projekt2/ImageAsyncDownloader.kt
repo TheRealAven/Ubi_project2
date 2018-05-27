3cb6c209e39bd61ebba7934987ff4080e11375b6
travis-ci <travis@travis-ci.org>
2018-05-27 21:49:50 +0200

    Funkcjonalna aplikacja v1.0

---
+++ app/src/main/java/com/example/aven/projekt2/ImageAsyncDownloader.kt
@@ -0,0 +1,7 @@
+package com.example.aven.projekt2
+
+/**
+ * Created by Aven on 2018-05-27.
+ */
+class ImageAsyncDownloader {
+}
---
+++ app/src/main/java/com/example/aven/projekt2/ProjectListAdapter.kt
@@ -0,0 +1,7 @@
+package com.example.aven.projekt2
+
+/**
+ * Created by Aven on 2018-05-26.
+ */
+class ProjectListAdapter {
+}
